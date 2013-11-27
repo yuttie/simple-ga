ead936469069a9ffec72f5ce6f17f88df52c3f49
Yuta Taniguchi <yuta.taniguchi.y.t@gmail.com>
2013-11-27 14:15:55 +0900

    Remove an unused language extension

--- test-ga.hs
+++ test-ga.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE TypeFamilies          #-}
 module Main (main) where
 
 import           Control.Applicative
